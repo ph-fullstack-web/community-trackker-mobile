0f0580912ddf5f89ebe906f9ee8ec86c2d5b49b0
milyopena <mily.opena@gmail.com>
2023-02-13 15:14:16 +0800

    feat(added context to Communities List)

--- src/providers/CommunitiesDataProvider/CommunitiesDataProvider.tsx
+++ src/providers/CommunitiesDataProvider/CommunitiesDataProvider.tsx
@@ -24,7 +24,45 @@ export const CommunitiesDataProvider = ({
       const mockData: Community[] = [
         {
           communityId: '00001',
-          name: 'Full Stack Web',
+          name: 'Community AAA',
+          managerName: 'Manager AAA',
+          totalMembers: 10,
+        },
+        {
+          communityId: '00002',
+          name: 'Community BBB',
+          managerName: 'Manager BBB',
+          totalMembers: 20,
+        },
+        {
+          communityId: '00003',
+          name: 'Community CCC',
+          managerName: 'Manager CCC',
+          totalMembers: 30,
+        },
+        {
+          communityId: '00004',
+          name: 'Community DDD',
+          managerName: 'Manager DDD',
+          totalMembers: 40,
+        },
+        {
+          communityId: '00005',
+          name: 'Community EEE',
+          managerName: 'Manager EEE',
+          totalMembers: 50,
+        },
+        {
+          communityId: '00006',
+          name: 'Community FFF',
+          managerName: 'Manager FFF',
+          totalMembers: 60,
+        },
+        {
+          communityId: '00007',
+          name: 'Community GGG',
+          managerName: 'Manager GGG',
+          totalMembers: 70,
         },
       ];
 
