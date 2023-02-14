900307984065818425f1acd48c74bc60fe0a5737
vpableo <vincent.pableo@softvision.com>
2023-02-14 17:47:13 +0800

    Added prototype expandable view

--- src/components/organisms/MemberDetailForm/index.tsx
+++ src/components/organisms/MemberDetailForm/index.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ReactNode, useEffect, useState } from "react";
+import { Animated, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Button } from "components/atoms";
 import { LabeledInput } from "components/molecules";
 
 export const MemberDetailForm = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const expandedText = isExpanded ? 'Less' : 'More' 
+
   return (
     <>
       <SafeAreaView style={{height: '100%', paddingBottom: 60}}>
@@ -12,16 +16,27 @@ export const MemberDetailForm = () => {
             <View style={styles.form_header}>
               <Text>Employee Name</Text>
             </View>
-            <View style={{ paddingVertical: 10 }}>
+            <View>
               <DetailForm />
-              <View>
-                <DetailForm />
+              <View style={{ marginTop: 10 }}>
+
+                <TouchableOpacity
+                  onPress={() => {
+                    setIsExpanded(!isExpanded);
+                  }}
+                  style={styles.toggle}
+                >
+                  <Text style={styles.toggleText}>View {expandedText}</Text>
+                </TouchableOpacity>
+                <ExpandableView
+                  expanded={isExpanded}
+                  form={<DetailForm />}
+                />
               </View>
             </View>
           </View>
         </ScrollView>
       </SafeAreaView>
-
     </>
   )
 }
@@ -73,6 +88,31 @@ const DetailForm = () => {
   )
 }
 
+interface ExpandableViewProps {
+ expanded: boolean
+ form: ReactNode
+}
+
+const ExpandableView = (props: ExpandableViewProps) => {
+  const [height] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(height, {
+      toValue: props.expanded ? 300 : 0,
+      duration: 150,
+      useNativeDriver: false
+    }).start();
+  }, [props.expanded, height]);
+
+  return (
+    <Animated.View
+      style={{ height, backgroundColor: "#eee" }}
+    >
+      {props.form}
+    </Animated.View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 1,
@@ -87,5 +127,14 @@ const styles = StyleSheet.create({
   },
   community_button: {
     marginTop: 3
+  },
+  toggle: {
+    height: 30,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  toggleText: {
+    color: "#fff"
   }
 })
