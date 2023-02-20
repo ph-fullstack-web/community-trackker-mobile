01f5fa378e99c514480104102909b7bb05b65973
vpableo <vincent.pableo@softvision.com>
2023-02-20 10:33:13 +0800

    Added theme.js file

---
+++ theme/index.js
@@ -0,0 +1,46 @@
+const theme = {
+  COLOR: {
+    BASE:'#f3f6f8',
+    PRIMARY: '2f78c4',
+    SECONDARY: '#06c7cc',
+    SHADOW: '#141414'
+  },
+  FONT: {
+    FAMILY: 'Arial, Sans-serif',
+    SIZE: {
+      SM: 12,
+      MD: 14,
+      LG: 20,
+    },
+    WEIGHT: {
+      BOLD: 600,
+      LIGHT: 300,
+    }
+  },
+  SPACING: {
+    SM: 10,
+    MD: 15,
+    LG: 20
+  },
+  RADIUS: {
+    SM: 10,
+    MD: 15,
+    LG: 20,
+    CIRCLE: '50%',
+  },
+  MODE: {
+    LIGHT: {
+      COLOR: {
+        base:'#f3f6f8',
+      }
+    },
+    DARK : {
+      COLOR: {
+        BASE: '#141414',
+      }
+    }
+  },
+
+}
+
+export default theme;
