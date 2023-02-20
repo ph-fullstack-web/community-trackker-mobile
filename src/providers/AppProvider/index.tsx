283a9f7077f10f0153971b3f3d6653f802fb7029
vpableo <vincent.pableo@softvision.com>
2023-02-20 17:46:43 +0800

    Added ThemeProvider

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import {UserDataProvider} from 'providers/UserDataProvider';
+import {ThemeProvider} from 'providers/ThemeProviders';
+import theme from '../../../theme';
 
 type AppProvidersProps = ComponentWithChildren;
 
 export const AppProvider = ({children}: AppProvidersProps) => {
-  return <UserDataProvider>{children}</UserDataProvider>;
+  const [appTheme, setAppTheme] = useState(theme.LIGHT)
+  console.log("🚀 ~ file: index.tsx:10 ~ AppProvider ~ appTheme:", appTheme)
+  
+  return (
+    <UserDataProvider>
+      <ThemeProvider theme={appTheme}> 
+        {children}
+      </ThemeProvider>
+    </UserDataProvider>
+  );
 };
