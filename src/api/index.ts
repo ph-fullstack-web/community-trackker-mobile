33af3ccb0376b22f7d9cd6a00b91b8a7e30b3d45
milyopena <mily.opena@gmail.com>
2023-03-13 15:51:56 +0800

    feat(apply changes in base API url)

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ import {Platform} from 'react-native';
 export const communityTrackerAPI = axios.create({
   baseURL:
     Platform.OS === 'ios'
-      ? 'http://localhost:8000/api/'
+      ? 'http://127.0.0.1:8000/api/'
       : 'http://10.0.2.2:8000/api/',
   timeout: 60000,
 });
